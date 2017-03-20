6057090722bfccabe2610eae3a6e616867d62bb6
lutfy86 <lutfyvanginkel86@gmail.com>
2017-03-20 23:29:27 +0700

    Add files via upload

---
+++ temporary2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+//prototype
+void prosesBaca ();
+void prosesTukar () ;
+void prosesTampil () ;
+
+// variabel global
+int a,b,temp;
+//program utama
+int main () {
+	prosesBaca () ;
+	prosesTukar () ;
+	prosesTampil () ;
+	return 0;
+}
+//proses baca
+void prosesBaca () {
+	cout <<"masukkan nilai A : ";
+	cin >> a;
+	cout <<"masukkan nilai B : ";
+	cin >>b;
+}
+	//proses tukar
+void prosesTukar () {
+		temp = b;
+		b = a;
+		a = temp;
+	}
+	
+	//proses tampil
+void prosesTampil () {
+		cout <<"====================" <<endl;
+		cout <<"nilai A sekarang : "<< a <<endl;
+		cout <<"nilai B sekarang : " << b <<endl;
+		
+	}
+		
+		
+	
+	
+	
+
